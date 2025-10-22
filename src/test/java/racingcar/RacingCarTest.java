4b195e639f712b77fa48a54fae743f2f53cef262
jyt6640 <jyt6640@gmail.com>
2025-10-22 13:23:47 +0900

    test(RacingCarTest): 자동차 이름 5자 이상 작성 시 예외 발생 테스트 추가

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,7 +16,6 @@ public class RacingCarTest {
         racingCar = new RacingCar();
     }
 
-
     @DisplayName("자동차를 쉼표 기준으로 분리")
     @Test
     void 자동차를_쉼표를_기준으로_분리() {
@@ -27,5 +27,15 @@ public class RacingCarTest {
 
         //then
         assertArrayEquals(new String[]{"pobi", "woni", "jun"}, cars);
+    }
+
+    @DisplayName("자동차 이름 5자 이상 작성 시 예외 발생")
+    @Test
+    void 자동차_이름_5자_이상_작성_시_예외_발생() {
+        //given
+        String input = "pobijjang,woni,jun";
+
+        //when&then
+        assertThrows(IllegalArgumentException.class, () -> racingCar.createCars(input));
     }
 }
