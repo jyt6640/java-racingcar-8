2e3b277cbbf855133bfebef6afdd91fff7a8dd8a
jyt6640 <jyt6640@gmail.com>
2025-10-22 20:26:58 +0900

    test(RacingCarTest): 주어진 횟수가 0 이하일 경우 예외 발생 테스트 추가

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -126,4 +126,16 @@ public class RacingCarTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("시도 횟수는 숫자여야 합니다.");
     }
+
+    @DisplayName("주어진 횟수가 0 이하일 경우 예외 발생")
+    @Test
+    void 주어진_횟수가_0_이하일_경우_예외_발생() {
+        //given
+        String input = "0";
+
+        //when&then
+        assertThatThrownBy(() -> racingCar.validateAttemptCount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("시도 횟수는 1회 이상이여야 합니다.");
+    }
 }
