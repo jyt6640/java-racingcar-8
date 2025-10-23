01229f8e133e11d0972aede8b5f21864fd559fca
jyt6640 <jyt6640@gmail.com>
2025-10-23 13:01:11 +0900

    feat(Application): 경주 게임 전체 실행 흐름 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,30 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingCar racingCar = new RacingCar();
+
+        try {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String carInput = Console.readLine();
+            String[] cars = racingCar.createCars(carInput);
+            racingCar.initializeCars(cars);
+
+            System.out.println("시도할 횟수는 몇 회인가요?");
+            String attemptInput = Console.readLine();
+            int attemptCount = Integer.parseInt(attemptInput);
+
+            System.out.println("\n실행 결과");
+            racingCar.startRace(attemptCount);
+
+            System.out.println("최종 우승자 : " + racingCar.findWinners());
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR]" + e.getMessage());
+            throw e;
+        } finally {
+            Console.close();
+        }
     }
 }
