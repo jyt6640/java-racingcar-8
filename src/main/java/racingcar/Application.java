58d8b9a23dbf652fd21bdbc343351dddbff7f018
jyt6640 <jyt6640@gmail.com>
2025-10-23 15:06:04 +0900

    refactor(InputView): 입력 기능 InputView로 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,13 +7,11 @@ public class Application {
         RacingCar racingCar = new RacingCar();
 
         try {
-            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-            String carInput = Console.readLine();
+            String carInput = InputView.readCar();
             String[] cars = racingCar.createCars(carInput);
             racingCar.initializeCars(cars);
 
-            System.out.println("시도할 횟수는 몇 회인가요?");
-            String attemptInput = Console.readLine();
+            String attemptInput = InputView.readAttemptCount();
             int attemptCount = Integer.parseInt(attemptInput);
 
             System.out.println("\n실행 결과");
