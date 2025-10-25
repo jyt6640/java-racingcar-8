7f1b6eaf0ce0d7320d4aad377cf050cc4d1aeeed
jyt6640 <jyt6640@gmail.com>
2025-10-25 21:07:47 +0900

    feat(InputValidator): 자동차 이름 입력 정규식 기반으로 검증하는 기능 추가

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,6 +1,11 @@
 package racingcar.validator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class InputValidator {
+    private static final Pattern CAR_NAME_INPUT_PATTERN = Pattern.compile("^[^,]+(,[^,]*)*$");
+
     public void validateCarsNameNotBlank(String cars) {
         if (cars == null || cars.isBlank()) {
             throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
@@ -12,6 +17,14 @@ public class InputValidator {
             return;
         }
         throw new IllegalArgumentException("자동차는 2대 이상이어야 합니다.");
+    }
+
+    public void validateCarsNameFormat(String cars) {
+        Matcher matcher = CAR_NAME_INPUT_PATTERN.matcher(cars);
+        if(matcher.matches()) {
+            return;
+        }
+        throw new IllegalArgumentException("입력 형식이 잘못되었습니다.");
     }
 
     public int validateAttemptCount(String attemptCounts) {
