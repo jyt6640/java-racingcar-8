3da9b481d8252355362176f33408a408b6b81ca1
jyt6640 <jyt6640@gmail.com>
2025-10-25 20:28:19 +0900

    feat(InputValidator): 자동차를 한대만 입력 시 예외 발생 기능 구현

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,15 +1,22 @@
 package racingcar.validator;
 
 public class InputValidator {
-    public void validateCarsNameNotBlank(String Cars) {
-        if (Cars == null || Cars.isBlank()) {
+    public void validateCarsNameNotBlank(String cars) {
+        if (cars == null || cars.isBlank()) {
             throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
         }
     }
 
-    public int validateAttemptCount(String AttemptCount) {
+    public void validateMinimumCarCount(String cars) {
+        if (cars.contains(",")) {
+            return;
+        }
+        throw new IllegalArgumentException("자동차는 2대 이상이어야 합니다.");
+    }
+
+    public int validateAttemptCount(String attemptCounts) {
         try {
-            int attemptCount = Integer.parseInt(AttemptCount);
+            int attemptCount = Integer.parseInt(attemptCounts);
             validateAttemptCountRange(attemptCount);
             return attemptCount;
         } catch (NumberFormatException e) {
