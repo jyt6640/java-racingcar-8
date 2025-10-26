4e12cc926a7ab46c35a6544023555601879b8f4a
jyt6640 <jyt6640@gmail.com>
2025-10-26 17:05:30 +0900

    style(ErrorMessage): 오타 및 문법 수정

--- src/main/java/racingcar/constants/ErrorMessage.java
+++ src/main/java/racingcar/constants/ErrorMessage.java
@@ -6,7 +6,7 @@ public enum ErrorMessage {
     INVALID_CAR_COUNT("자동차는 2대 이상이어야 합니다."),
     INVALID_ATTEMPT_NOT_NUMBER("시도 횟수는 숫자이어야 합니다."),
     INVALID_ATTEMPT_INT_RANGE("시도 횟수는 int 범위를 넘어갈 수 없습니다."),
-    INVALID_ATTEMPT_RANGE("시도 횟수는 1회 이상이어야 합니다"),
+    INVALID_ATTEMPT_RANGE("시도 횟수는 1회 이상이어야 합니다."),
     DUPLICATE_CAR_NAME("자동차 이름은 중복될 수 없습니다."),
     INVALID_CAR_NAME_LENGTH("자동차 이름은 5글자를 초과할 수 없습니다."),
     INVALID_CAR_NAME_NOT_BLANK("자동차 이름은 공백일 수 없습니다.");
