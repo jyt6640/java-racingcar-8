5c76d9d8a5aefa193188e5ffc718eefc97c37f96
jyt6640 <jyt6640@gmail.com>
2025-10-26 17:21:13 +0900

    test(InputValidatorTest): 예외 케이스 추가하여 테스트

--- src/test/java/racingcar/validator/InputValidatorTest.java
+++ src/test/java/racingcar/validator/InputValidatorTest.java
@@ -22,11 +22,9 @@ public class InputValidatorTest {
     }
 
     @DisplayName("자동차 이름이 공백일 경우 예외 발생")
-    @Test
-    void 자동차_이름이_공백일_경우_예외_발생() {
-        //given
-        String input = "";
-
+    @ValueSource(strings = {""," ","  "})
+    @ParameterizedTest
+    void 자동차_이름이_공백일_경우_예외_발생(String input) {
         //when&then
         assertThatThrownBy(() -> inputValidator.validateCarsName(input))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -59,11 +57,9 @@ public class InputValidatorTest {
     }
 
     @DisplayName("주어진 횟수가 숫자가 아닐 경우 예외 발생")
-    @Test
-    void 주어진_횟수가_숫자가_아닐_경우_예외_발생() {
-        //given
-        String input = "five";
-
+    @ValueSource(strings = {"abc", "one", "1a", "a1", "1.5"})
+    @ParameterizedTest
+    void 주어진_횟수가_숫자가_아닐_경우_예외_발생(String input) {
         //when&then
         assertThatThrownBy(() -> inputValidator.validateAttemptCountFormat(input))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -71,11 +67,9 @@ public class InputValidatorTest {
     }
 
     @DisplayName("주어진 횟수가 0 이하일 경우 예외 발생")
-    @Test
-    void 주어진_횟수가_0_이하일_경우_예외_발생() {
-        //given
-        int input = 0;
-
+    @ValueSource(ints = {0, -1, -100, -512})
+    @ParameterizedTest
+    void 주어진_횟수가_0_이하일_경우_예외_발생(int input) {
         //when&then
         assertThatThrownBy(() -> inputValidator.validateAttemptCountRange(input))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -83,11 +77,9 @@ public class InputValidatorTest {
     }
 
     @DisplayName("주어진 횟수가 공백일 경우 예외 발생")
-    @Test
-    void 주어진_횟수가_공백일_경우_예외_발생() {
-        //given
-        String input = "";
-
+    @ValueSource(strings = {""," ","  "})
+    @ParameterizedTest
+    void 주어진_횟수가_공백일_경우_예외_발생(String input) {
         //when&then
         assertThatThrownBy(() -> inputValidator.validateAttemptCountFormat(input))
                 .isInstanceOf(IllegalArgumentException.class)
