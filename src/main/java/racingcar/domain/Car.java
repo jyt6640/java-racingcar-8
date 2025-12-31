867a2c250c57913dfb18e059a53136a12db790ff
jyt6640 <jyt6640@gmail.com>
2025-12-31 16:45:21 +0900

    feat: 자동차 이름 예외 처리 기능 구현
    - 자동차 이름 길이가 5자 초과일 경우 예외 발생
    - 자동차 이름이 공백인 경우 예외 발생

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -6,6 +6,7 @@ public class Car {
     private int position;
 
     public Car(String name) {
+        validate(name);
         this.name = name;
         this.position = 0;
     }
@@ -20,5 +21,22 @@ public class Car {
 
     public int getPosition() {
         return position;
+    }
+
+    private void validate(String name) {
+        empty(name);
+        length(name);
+    }
+
+    private void length(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름 길이가 5자 초과입니다.");
+        }
+    }
+
+    private void empty(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름이 공백입니다.");
+        }
     }
 }
