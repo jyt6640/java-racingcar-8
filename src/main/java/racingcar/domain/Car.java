4cc3c2f980e7e75f185ed7800046bcb78af89961
jyt6640 <jyt6640@gmail.com>
2025-12-31 15:24:58 +0900

    feat: 자동차를 관리하는 자동차 도메인 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,12 @@
+package racingcar.domain;
+
+public class Car {
+
+    private String name;
+    private int position;
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+}
